384d65aa6cd06c98028bfa1447517e47755e58c0
gitisfine <npathare@purdue.edu>
2025-11-12 02:27:21 -0500

    New Layout

--- ShopAssist/app/build.gradle.kts
+++ ShopAssist/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose) apply false
 }
 
 android {
@@ -13,10 +14,8 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -39,7 +38,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
