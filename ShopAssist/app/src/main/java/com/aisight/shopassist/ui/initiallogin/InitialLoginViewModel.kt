cf8e87b94e6012502147d7ece8cb18d0e539201e
gitisfine <npathare@purdue.edu>
2025-11-15 13:23:52 -0500

    New Layout

--- ShopAssist/app/src/main/java/com/aisight/shopassist/ui/initiallogin/InitialLoginViewModel.kt
+++ ShopAssist/app/src/main/java/com/aisight/shopassist/ui/initiallogin/InitialLoginViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class InitialLoginViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is page"
+        value = "UserName"
     }
     val text: LiveData<String> = _text
 }
